30475d4ec2ddb43034949cb96b7750feab7ffde2
SmpManeth <smpmaneth@gmail.com>
2025-04-30 15:55:10 +0530

    update navigation.blade.php and web.php

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,13 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
-    Route::get('/new-dashboard', function () {
-        return view('new-dashboard');
-    })->name('new-dashboard');
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
