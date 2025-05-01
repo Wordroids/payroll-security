dc74b7fb8ac3953a7debdf39b7421942ce2449af
SmpManeth <smpmaneth@gmail.com>
2025-05-01 16:32:47 +0530

    update navigation.blade.php and sidebar.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,58 +1,55 @@
-<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-    <div class="px-3 py-3 lg:px-5 lg:pl-3">
-        <div class="flex items-center justify-between">
-            <div class="flex items-center justify-start rtl:justify-end">
-                <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-                    type="button"
-                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                    <span class="sr-only">Open sidebar</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path clip-rule="evenodd" fill-rule="evenodd"
-                            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                        </path>
-                    </svg>
-                </button>
-                <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24">
-                    <img src="{{ asset('images/logo.png') }}" class="w-10 me-3" alt="FlowBite Logo" />
-                    <span
-                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white uppercase">Smart Syndicates</span>
-                </a>
-            </div>
-            <div class="flex items-center">
-                <div class="flex items-center ms-3">
-                    <div>
-                        <button type="button"
-                            class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                            aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
-                        </button>
-                    </div>
-                    <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600"
-                        id="dropdown-user">
-                        <div class="px-4 py-3" role="none">
-                            <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                {{ auth()->user()->name }}
-                            </p>
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                {{ auth()->user()->email }}
-                            </p>
-                        </div>
-                        <ul class="py-1" role="none">
+<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 shadow-sm">
+    <div class="flex items-center justify-between px-4 py-3 lg:px-6">
+        {{-- Left Section --}}
+        <div class="flex items-center">
+            {{-- Mobile Toggle Button --}}
+            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar"
+                aria-controls="logo-sidebar"
+                type="button"
+                class="inline-flex items-center p-2 text-gray-600 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-300">
+                <span class="sr-only">Open sidebar</span>
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm.75 4.75a.75.75 0 000 1.5h7.5a.75.75 0 000-1.5h-7.5z" />
+                </svg>
+            </button>
 
-                            <li>
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button type="submit"
-                                        class="block px-4 py-2 text-sm text-red-700 hover:bg-red-100 dark:text-red-300 dark:hover:bg-gray-600 dark:hover:text-white">
-                                        Sign out
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
+            {{-- Logo + Brand --}}
+            <a href="{{ route('dashboard') }}" class="flex items-center ms-3 space-x-2">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-9 h-9">
+                <span class="text-lg font-semibold text-gray-800 uppercase">Smart Syndicates</span>
+            </a>
+        </div>
+
+        {{-- Right Section --}}
+        <div class="relative">
+            <button type="button"
+                class="flex items-center space-x-2 focus:outline-none"
+                data-dropdown-toggle="dropdown-user">
+                <img class="w-9 h-9 rounded-full border border-gray-300"
+                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="User Avatar">
+                <span class="hidden sm:inline-block text-sm text-gray-700 font-medium">
+                    {{ auth()->user()->name }}
+                </span>
+            </button>
+
+            {{-- Dropdown Menu --}}
+            <div id="dropdown-user"
+                class="hidden absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-md z-50">
+                <div class="px-4 py-3 text-blue-600 hover:bg-blue-50">
+                    <p class="text-sm font-semibold text-gray-900"><a href="{{route('profile.edit')}}">
+                        {{ auth()->user()->name }}</a> </p>
+                    <p class="text-sm text-gray-600 truncate"><a href="{{route('profile.edit')}}">
+                        {{ auth()->user()->email }}</a></p>
+                </div>
+                <div class="border-t border-gray-100">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit"
+                            class="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50">
+                            Sign out
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,151 +1,113 @@
 <aside id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
     aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
+    <div class="h-full px-4 pb-6 overflow-y-auto bg-white">
+        <ul class="space-y-2 text-sm font-medium text-gray-700">
 
-
-            {{-- Employees  --}}
+            {{-- Section: Guards --}}
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd"/>
-                      </svg>
-                      
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Guards</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
+                    class="flex items-center w-full px-2 py-2 text-left rounded-lg hover:bg-gray-100 transition group"
+                    aria-controls="dropdown-guards" data-collapse-toggle="dropdown-guards">
+                    <svg class="w-5 h-5 text-gray-500 group-hover:text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8 0a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.9 4a3 3 0 0 0-2.84 2.05l-.05.15c-.03.09-.04.12-.03.15.01.02.03.04.07.07a1 1 0 0 0 .31.28H12a1 1 0 0 0 .81-.41l.07-.07c.01-.03 0-.06-.03-.15l-.04-.12A3 3 0 0 0 10.1 13H7.9Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <span class="ml-3 flex-1">Guards</span>
+                    <svg class="w-3 h-3 ml-auto text-gray-400 group-hover:text-indigo-600" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
+                            d="M1 1l4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                <ul id="dropdown-guards" class="hidden ml-7 mt-1 space-y-1">
                     <li>
                         <a href="{{ route('employees.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">All
-                            Guards</a>
+                            class="block px-2 py-1 rounded hover:bg-gray-100 transition">All Guards</a>
                     </li>
-
                 </ul>
             </li>
 
-            {{-- Sites --}}
+            {{-- Section: Sites --}}
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="sites-dropdown" data-collapse-toggle="sites-dropdown">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M3 21h18M4 18h16M6 10v8m4-8v8m4-8v8m4-8v8M4 9.5v-.955a1 1 0 0 1 .458-.84l7-4.52a1 1 0 0 1 1.084 0l7 4.52a1 1 0 0 1 .458.84V9.5a.5.5 0 0 1-.5.5h-15a.5.5 0 0 1-.5-.5Z"/>
-                      </svg>
-                      
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Sites</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
+                    class="flex items-center w-full px-2 py-2 text-left rounded-lg hover:bg-gray-100 transition group"
+                    aria-controls="dropdown-sites" data-collapse-toggle="dropdown-sites">
+                    <svg class="w-5 h-5 text-gray-500 group-hover:text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-width="2" d="M3 21h18M4 18h16M6 10v8m4-8v8m4-8v8m4-8v8M4 9.5v-.95a1 1 0 0 1 .45-.85l7-4.5a1 1 0 0 1 1.1 0l7 4.5a1 1 0 0 1 .45.85v.95a.5.5 0 0 1-.5.5h-15a.5.5 0 0 1-.5-.5Z"/>
+                    </svg>
+                    <span class="ml-3 flex-1">Sites</span>
+                    <svg class="w-3 h-3 ml-auto text-gray-400 group-hover:text-indigo-600" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
+                            d="M1 1l4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="sites-dropdown" class="hidden py-2 space-y-2">
+                <ul id="dropdown-sites" class="hidden ml-7 mt-1 space-y-1">
                     <li>
                         <a href="{{ route('sites.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            All Sites</a>
+                            class="block px-2 py-1 rounded hover:bg-gray-100 transition">All Sites</a>
                     </li>
-
                 </ul>
             </li>
 
-            {{-- Mark Attendance --}}
+            {{-- Section: Attendance --}}
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="attendance-dropdown" data-collapse-toggle="attendance-dropdown">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 6H5m2 3H5m2 3H5m2 3H5m2 3H5m11-1a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2M7 3h11a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Zm8 7a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
-                      </svg>
-                      
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Attendance</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
+                    class="flex items-center w-full px-2 py-2 text-left rounded-lg hover:bg-gray-100 transition group"
+                    aria-controls="dropdown-attendance" data-collapse-toggle="dropdown-attendance">
+                    <svg class="w-5 h-5 text-gray-500 group-hover:text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M7 6H5m2 3H5m2 3H5m2 3H5m2 3H5m11-1a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2M7 3h11a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Zm8 7a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                    </svg>
+                    <span class="ml-3 flex-1">Attendance</span>
+                    <svg class="w-3 h-3 ml-auto text-gray-400 group-hover:text-indigo-600" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
+                            d="M1 1l4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="attendance-dropdown" class="hidden py-2 space-y-2">
-                    <li>
-                        <a href="{{ route('attendances.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            All Attendances</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('attendances.site-entry') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            Mark Attendances</a>
-                    </li>
-
+                <ul id="dropdown-attendance" class="hidden ml-7 mt-1 space-y-1">
+                    <li><a href="{{ route('attendances.index') }}" class="block px-2 py-1 rounded hover:bg-gray-100 transition">All Attendances</a></li>
+                    <li><a href="{{ route('attendances.site-entry') }}" class="block px-2 py-1 rounded hover:bg-gray-100 transition">Mark Attendance</a></li>
                 </ul>
             </li>
 
-            {{-- Salary --}}
+            {{-- Section: Salaries --}}
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="salary-dropdown" data-collapse-toggle="salary-dropdown">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    class="flex items-center w-full px-2 py-2 text-left rounded-lg hover:bg-gray-100 transition group"
+                    aria-controls="dropdown-salaries" data-collapse-toggle="dropdown-salaries">
+                    <svg class="w-5 h-5 text-gray-500 group-hover:text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M7 6a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-2v-4a3 3 0 0 0-3-3H7V6Z" clip-rule="evenodd"/>
                         <path fill-rule="evenodd" d="M2 11a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-7Zm7.5 1a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Z" clip-rule="evenodd"/>
                         <path d="M10.5 14.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/>
-                      </svg>
-                      
-                      
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Salaries</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
+                    </svg>
+                    <span class="ml-3 flex-1">Salaries</span>
+                    <svg class="w-3 h-3 ml-auto text-gray-400 group-hover:text-indigo-600" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
+                            d="M1 1l4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="salary-dropdown" class="hidden py-2 space-y-2">
-                    <li>
-                        <a href="{{ route('salary.advance') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            Salary Advances</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('salaries') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            Salaries</a>
-                    </li>
-
+                <ul id="dropdown-salaries" class="hidden ml-7 mt-1 space-y-1">
+                    <li><a href="{{ route('salary.advance') }}" class="block px-2 py-1 rounded hover:bg-gray-100 transition">Salary Advances</a></li>
+                    <li><a href="{{ route('salaries') }}" class="block px-2 py-1 rounded hover:bg-gray-100 transition">Salaries</a></li>
                 </ul>
             </li>
 
-             {{-- Users  --}}
-             <li>
+            {{-- Section: Users --}}
+            <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="user-dropdown" data-collapse-toggle="user-dropdown">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    class="flex items-center w-full px-2 py-2 text-left rounded-lg hover:bg-gray-100 transition group"
+                    aria-controls="dropdown-users" data-collapse-toggle="dropdown-users">
+                    <svg class="w-5 h-5 text-gray-500 group-hover:text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
-                      </svg>
-                      
-                      
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Users</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
+                    </svg>
+                    <span class="ml-3 flex-1">Users</span>
+                    <svg class="w-3 h-3 ml-auto text-gray-400 group-hover:text-indigo-600" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
+                            d="M1 1l4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="user-dropdown" class="hidden py-2 space-y-2">
-                    <li>
-                        <a href="{{ route('users.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">All
-                            Users</a>
-                    </li>
-
+                <ul id="dropdown-users" class="hidden ml-7 mt-1 space-y-1">
+                    <li><a href="{{ route('users.index') }}" class="block px-2 py-1 rounded hover:bg-gray-100 transition">All Users</a></li>
                 </ul>
             </li>
         </ul>
